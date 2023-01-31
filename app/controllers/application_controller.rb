6ee3e2c0762b864094adb9723bd0be95388ae7f4
nhh2 <nhernand@chicagobooth.edu>
2023-01-31 14:59:54 +0000

    First pause in video

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+    render ({ :template => "dashboard_templates/homepage.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# Define the "/homepage" URL route
+  get("/", { :controller => "application", :action => "homepage"})
+
+# Define the "/forex" URL route
+  get("/forex", { :controller => "currencies", :action => "first_currency"})
+
 end
